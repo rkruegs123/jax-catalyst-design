20d91b26c9c1894734fafd7c3312f01d6494488f
Ryan Krueger <ryankrue@umich.edu>
2024-07-20 22:20:19 -0400

    Split point for tagged as well

--- catalyst/icosahedron_tagged/wham.py
+++ catalyst/icosahedron_tagged/wham.py
@@ -94,6 +94,9 @@ def run(args, sim_params):
     n_bins = args['n_bins']
     op_name = args['op_name']
 
+    use_split_point = args['use_split_point']
+    split_point = args['split_point']
+
     wham_basedir = Path(args['wham_basedir'])
     assert(wham_basedir.exists() and wham_basedir.is_dir())
     wham_exec_path = wham_basedir / "wham" / "wham"
@@ -233,16 +236,31 @@ def run(args, sim_params):
 
     min_center = args['min_center']
     max_center = args['max_center']
-    bin_centers = list(onp.linspace(min_center, max_center, n_bins))
-    num_centers = len(bin_centers)
-    bin_centers = jnp.array(bin_centers)
-
-    k_biases = list()
-    # k_bias = 5e4
     k_bias = args['k_bias']
-    for center in bin_centers:
-        k_biases.append(k_bias)
-    k_biases = jnp.array(k_biases)
+
+    if not use_split_point:
+        bin_centers = list(onp.linspace(min_center, max_center, n_bins))
+        num_centers = len(bin_centers)
+        bin_centers = jnp.array(bin_centers)
+
+        k_biases = list()
+
+        for center in bin_centers:
+            k_biases.append(k_bias)
+        k_biases = jnp.array(k_biases)
+    else:
+        assert(split_point > min_center and split_point < max_center)
+        k_bias_split_point = args['k_bias_split_point']
+        bin_centers_lo = list(onp.linspace(min_center, split_point, n_bins))
+        k_biases_lo = [k_bias_split_point for _ in range(n_bins)]
+
+        bin_centers_hi = list(onp.linspace(split_point, max_center, n_bins))
+        k_biases_hi = [k_bias for _ in range(n_bins)]
+
+        bin_centers = jnp.array(bin_centers_lo + bin_centers_lo)
+        k_biases = jnp.array(k_biases_lo + k_biases_lo)
+        num_centers = len(bin_centers)
+        n_bins *= 2
 
 
     if op_name == "attr":
@@ -535,6 +553,12 @@ def get_parser():
     parser.add_argument('--op-name', type=str, help='Name of order parameter',
                         choices=["attr", "head"],
                         default="attr")
+
+    parser.add_argument('--use-split-point', action='store_true')
+    parser.add_argument('--split-point', type=float, default=4.0,
+                        help="Point for splitting the centers.")
+    parser.add_argument('--k-bias-split-point', type=float, default=50000.0,
+                        help="Spring constant for centers between min_center and split_point.")
 
     return parser
 
